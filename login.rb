9028a5cd948826959b789d55631edaae1f9e17ad
Joel Kottas <sumnon@cj3.org>
2014-11-06 20:53:36 -0500

    Put student ID and password in cmd line args

--- login.rb
+++ login.rb
@@ -2,21 +2,13 @@ require 'mechanize'
 
 agent = Mechanize.new
 agent.ssl_version = 'SSLv3'
-
-puts "WARNING: This program needs to disable SSL verification to work."
-puts "Be sure your browser can open pages with SSL safely first."
 agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 page = agent.get("https://webreg.its.rochester.edu/prod/web/LoginMain.jsp")
 
 form = page.form_with(:name => "Form")
 
-puts "Please enter your student ID:"
-form["STUDENT_ID"] = STDIN.gets.chomp
-
-puts "Please enter your password:"
-`stty -echo`
-form["STUDENT_PIN"] = STDIN.gets.chomp
-`stty echo`
+form["STUDENT_ID"] = ARGV[0]
+form["STUDENT_PIN"] = ARGV[1]
 
 form.submit
